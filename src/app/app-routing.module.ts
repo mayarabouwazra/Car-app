4d0c984529c2e1f95a3ab0712a677c3116e76df3
Mayara Bouwazra <mayarabouazra@gmail.com>
2024-07-17 14:10:39 +0100

    BugFix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
-import { FavoritesComponent } from './favorites/favorites.component';
 import { ListVehiclesComponent } from './list-vehicles/list-vehicles.component';
 
 
 const routes: Routes = [
   { path: 'main-table', component: ListVehiclesComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: 'favorites-page', component: FavoritesPageComponent },
   { path: '', redirectTo: '/main-table', pathMatch: 'full' },
 ];
 
